4aa4810da6771c13147cab6b8ad51cfa6504f26b
SaumyaBandara0722 <saumya19970722@gmail.com>
2020-05-02 10:20:23 +0530

    initial project created

---
+++ Online_Appointment2/src/model/Appointment.java
@@ -0,0 +1,207 @@
+package model;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import bean.AppointmentBean;
+import util.DBConnection;
+
+public class Appointment {
+	
+	Schedule sch = new Schedule();
+	DBConnection dbObj = new DBConnection();
+		
+	//method to insert data
+	public String insertDetails(String PatientID, String DueDate, int ScheduleId) {
+		String output = "";
+		try {
+			Connection con = dbObj.connect();
+			if (con == null) {
+				return "Error while connecting to the database";
+			}
+			// create a prepared statement
+			String query = " insert into appointment_doctor(`appointmentId`,`patientId`,`dueDate`,`scheduleId`)"
+					+ " values (?, ?, ?, ?)";
+			PreparedStatement preparedStmt = con.prepareStatement(query);
+			// binding values
+			preparedStmt.setInt(1, 0);
+			preparedStmt.setString(2, PatientID);
+			preparedStmt.setString(3, DueDate);
+			preparedStmt.setInt(4, ScheduleId);
+			//preparedStmt.setBoolean(5, false);
+									
+			// execute the statement 			
+			preparedStmt.execute();
+			output = "Inserted successfully";
+			
+		} catch (Exception e) {
+			output = "Error while inserting.Can't add a child row";
+			System.err.println(e.getMessage());
+		}
+		return output;
+	}
+
+	
+	//method to read database
+	public String readDetails() {
+		String output = "";
+		AppointmentBean docbean = new AppointmentBean();
+		
+		try {
+			Connection con = dbObj.connect();
+			if (con == null) {
+				return "Error while connecting to the database for reading.";
+			}
+			// Prepare the html table to be displayed
+			output = "<table border=\"1\"><tr><th>Appointment ID</th>" + "<th>Patient ID</th>" 
+					 + "<th>Date</th>" + "<th>Schedule Id</th></tr>";
+			String query = "select * from appointment_doctor";
+			Statement stmt = con.createStatement();
+			ResultSet rs = stmt.executeQuery(query);
+			// iterate through the rows in the result set
+			while (rs.next()) {
+				String AppointmentID = Integer.toString(rs.getInt("appointmentId"));
+				String PatientID = rs.getString("patientId");
+				String DueDate =rs.getString("dueDate");
+				String ScheduleId = Integer.toString(rs.getInt("scheduleId"));
+				//String Status = rs.getString("status");
+				
+				// Add into the html table
+				output += "<tr><td>" + AppointmentID + "</td>";
+				output += "<td>" + PatientID + "</td>";
+				output += "<td>" + DueDate + "</td>";
+				output += "<td>" + ScheduleId + "</td>";
+				//output += "<td>" + Status + "</td>";
+			}
+			con.close();
+			// Complete the html table
+			output += "</table>";
+		} catch (Exception e) {
+			output = "Error while reading the details.";
+			System.err.println(e.getMessage());
+		}
+		return output;
+	}
+
+	//method to update details
+	public String updateDetails(String AppointmentID, String PatientID,  String DueDate, String ScheduleId)
+	 {
+	 String output = "";
+	 try
+	 {
+	 Connection con = dbObj.connect();
+	 if (con == null)
+	 {return "Error while connecting to the database for updating."; }
+	 // create a prepared statement
+	 String query = "UPDATE appointment_doctor SET patientId=?,dueDate=?,ScheduleId=?WHERE appointmentId=?"; 
+	 PreparedStatement preparedStmt = con.prepareStatement(query);
+	 // binding values
+	 preparedStmt.setString(1, PatientID);
+	 preparedStmt.setString(2, DueDate);
+	 preparedStmt.setString(3, ScheduleId); 
+	 preparedStmt.setInt(4, Integer.parseInt(AppointmentID));
+	 // execute the statement
+	 preparedStmt.execute();
+	 con.close();
+	 output = "Updated successfully";
+	 }
+	 catch (Exception e)
+	 {
+	 output = "Error while updating the details.Can't update a child row";
+	 System.err.println(e.getMessage());
+	 }
+	 return output;
+	 }
+
+	//method to delete details
+	public String deleteDetails(String AppointmentID) {
+		String output = "";
+		try {
+			Connection con = dbObj.connect();
+			if (con == null) {
+				return "Error while connecting to the database for deleting.";
+			}
+			// create a prepared statement
+			String query = "delete from appointment_doctor where AppointmentID=?";
+			PreparedStatement preparedStmt = con.prepareStatement(query);
+			// binding values
+			preparedStmt.setInt(1, Integer.parseInt(AppointmentID));
+			// execute the statement
+			preparedStmt.execute();
+			con.close();
+			output = "Deleted successfully";
+		} catch (Exception e) {
+			output = "Error while deleting the details.";
+			System.err.println(e.getMessage());
+		}
+		return output;
+	}
+	
+	//search appointments by ID	
+	
+	// view list of appointments
+	public List<AppointmentBean> viewAppointments() {		
+		return	viewAppointments(null);
+	}
+	
+	// show appointments by ID
+	public AppointmentBean ShowAppointmentById(String id) {
+	List<AppointmentBean> list =viewAppointments(id);
+		if(!list.isEmpty()) {
+			return	list.get(0);
+		}
+		return null;
+	}
+			
+	//view method
+	public List<AppointmentBean> viewAppointments(String id) {
+		List <AppointmentBean> AppList = new ArrayList<>();
+		try 
+		{
+			Connection con = dbObj.connect();
+			if (con == null) {
+				System.out.println("Error While reading from database");
+				return AppList;
+			}
+
+			String query;
+			
+			if(id==null) {
+			query = "select * from appointment_doctor";
+			}
+			else {
+				query = "select * from appointment_doctor where patientId="+id;	
+			}
+			Statement stmt = con.createStatement();
+			ResultSet results = stmt.executeQuery(query);
+
+			while (results.next()) {
+				AppointmentBean type = new AppointmentBean(
+					results.getInt("appointmentid"),
+					results.getString("patientid"),
+					results.getString("dueDate"),
+					results.getInt("scheduleid")
+					//results.getBoolean("status")
+				);
+				AppList.add(type);
+			}
+			con.close();
+		}
+		catch (Exception e) {
+			System.out.println("Error While Reading");
+			System.err.println(e.getMessage());
+		}		
+		return AppList;
+	}
+	
+	public List<AppointmentBean> View_Appointments_By_given_ID(String pid) {
+		List<AppointmentBean> ScheduleBeanlist = new ArrayList<>();
+		for (AppointmentBean sch : viewAppointments()) {
+			if (pid.equals(sch.getPatientid())) {				
+				ScheduleBeanlist.add(sch);
+			}
+		}
+		return ScheduleBeanlist;
+	}
+}
---
+++ Online_Appointment2/src/model/Schedule.java
@@ -0,0 +1,72 @@
+package model;
+
+import bean.ScheduleBean;
+import util.DBConnection;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.sql.Time;
+
+public class Schedule{	
+	DBConnection dbObj = new DBConnection();
+	
+	// View Doctor Schedule
+	public String viewAllSchedule() {
+
+		String output = "";
+		ScheduleBean docbean = new ScheduleBean();
+		try {
+			Connection con = dbObj.connect();
+			if (con == null) {
+				return "Error while connecting to the database for reading.";
+			}
+			// Prepare the html table to be displayed
+			output = "<table border=\"1\"><tr><th>Schedule Id</th>"
+					+ "<th>Appointment Specification</th>"
+					+ "<th>Doctor</th>"
+					+ "<th>Hospital</th>"
+					+ "<th>Date</th>" 
+					+ "<th>Start Time</th>" 
+					+ "<th>End Time</th></tr>";
+
+			String query = "select s.Schedule_id,t.Appointment_Type,d.DoctorName,d.HospitalName,s.Date,s.Start_Time,s.End_Time from doctors d join appointment_scheduling s on s.D_id=d.DoctorID join appointment_type t on t.appointment_Id=s.App_id order by t.Appointment_Type";
+			Statement stmt = con.createStatement();
+			ResultSet rs = stmt.executeQuery(query);
+
+			// iterate through the rows in the result set
+			while (rs.next()) {
+				String schedule_id = rs.getString("Schedule_id");
+				String app_type = rs.getString("Appointment_Type");
+				String d_name = rs.getString("DoctorName");
+				String h_name = rs.getString("HospitalName");
+				String date = rs.getString("Date");
+				String start_time = rs.getString("Start_Time");
+				String end_time = rs.getString("End_Time");
+				
+				
+				// Add into the html table
+				output += "<tr><td>" + schedule_id + "</td>";
+				output += "<td>" + app_type + "</td>";
+				output += "<td>" + d_name + "</td>";
+				output += "<td>" + h_name + "</td>";
+				output += "<td>" + date + "</td>";
+				output += "<td>" + start_time + "</td>";
+				output += "<td>" + end_time + "</td></tr>";
+			}
+
+			con.close();
+			// Complete the html table
+			output += "</tr></table>";
+
+		} catch (Exception e) {
+			output = "Error while reading the Doctors Details.";
+			System.err.println(e.getMessage());
+		}
+
+		return output;
+	}
+}
